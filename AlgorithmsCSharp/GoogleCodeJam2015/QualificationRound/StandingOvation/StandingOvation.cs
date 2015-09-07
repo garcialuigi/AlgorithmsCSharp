910b9d4814b2d238d0e9ba16fb39278f3dc80e10
Luigi Garcia <mr.garcialuigi@gmail.com>
2015-09-07 00:03:18 -0300

    First Commit

---
+++ AlgorithmsCSharp/GoogleCodeJam2015/QualificationRound/StandingOvation/StandingOvation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoogleCodeJam2015.QualificationRound.StandingOvation
+{
+    class StandingOvation
+    {
+        public void Start()
+        {
+            int T = RequestInput<int>();
+            for (int t = 1; t <= T; t++)
+            {
+                string str = RequestInput<string>();
+                string[] strs = str.Split(' ');
+                LinkedList<int> list = new LinkedList<int>();
+                foreach (char item in strs[1])
+                    list.AddLast(item - '0');
+
+                int freeFriends = 0;
+                int friends = 0;
+                while (list.Count > 0)
+                {
+                    if (list.First.Value == 0)
+                    {
+                        list.RemoveFirst();
+                        if (freeFriends > 0)
+                            freeFriends--;
+                        else
+                            friends++;
+                    }
+                    else
+                    {
+                        freeFriends += list.First.Value - 1;
+                        list.RemoveFirst();
+                    }
+                }
+                Print("Case #" + t + ": " + friends + "\n");
+            }
+        }
+
+        public void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public T RequestInput<T>()
+        {
+            string input = "";
+
+            input = Console.ReadLine();
+
+            if (typeof(T) == typeof(string))
+                return (T)(object)input;
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+    }
+}
---
+++ AlgorithmsCSharp/MITbook/2_1_insertion_sort/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MITbook._2_1_insertion_sort
+{
+    class Class1
+    {
+        public void Start()
+        {
+            //int[] array = InsertionSort(new int[] { 3, 6, 5, 7, 1 });
+            int[] array = InsertionSort(new int[] { 3, 6, 5, 7, 1 }, true);
+            foreach (int item in array)
+                Console.WriteLine(item);
+
+            Console.WriteLine(LinearSearch(new int[] { 9, 5, 4 }, 5));
+        }
+
+        public int[] InsertionSort(int[] input, bool desc = false)
+        {
+            Func<int, int, bool> LoopComparer = null;
+            if (desc)
+                LoopComparer = (x, y) => x < y;
+            else
+                LoopComparer = (x, y) => x > y;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                int key = input[i];
+                int j = i - 1;
+                while (j >= 0 && LoopComparer(input[j], key))
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+                input[j + 1] = key;
+            }
+            return input;
+        }
+
+        public int? LinearSearch(int[] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                    return i;
+            }
+
+            return null;
+        }
+
+    }
+}
